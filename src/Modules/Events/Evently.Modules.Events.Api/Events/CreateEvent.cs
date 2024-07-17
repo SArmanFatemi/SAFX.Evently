16773c335b9fe8308d72239b7c521ebe3cb80cb4
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-07-17 15:14:43 +0200

    task: Implement create event usecase

---
+++ src/Modules/Events/Evently.Modules.Events.Api/Events/CreateEvent.cs
@@ -0,0 +1,33 @@
+using Evently.Modules.Events.Api.Database;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Events.Api.Events;
+
+public static class CreateEvent
+{
+	public static void MapEndpoint(IEndpointRouteBuilder app)
+	{
+		app.MapPost("evnents", async (Request request, EventDbContext dbContext) =>
+			{
+				var @event = new Event
+				{
+					Id = Guid.NewGuid(),
+					Title = request.Title,
+					Description = request.Description,
+					StartAtUsc = request.StartAtUtc,
+					EndAtUtc = request.EndAtUtc,
+					Status = EventStatus.Draft
+				};
+
+				dbContext.Events.Add(@event);
+				await dbContext.SaveChangesAsync();
+
+				return Results.Ok(@event.Id);
+			})
+			.WithTags(Tags.Events);
+	}
+
+	internal sealed record Request(string Title, string Description, DateTime StartAtUtc, DateTime EndAtUtc);
+}
