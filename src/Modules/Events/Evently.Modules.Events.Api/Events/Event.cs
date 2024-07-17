9764f4fbe7749961447cefc0ca5fa3a98277f8eb
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-07-17 14:55:17 +0200

    task: Implement event schema

---
+++ src/Modules/Events/Evently.Modules.Events.Api/Events/Event.cs
@@ -0,0 +1,18 @@
+namespace Evently.Modules.Events.Api.Events;
+
+public sealed class Event
+{
+	public Guid Id { get; set; }
+
+	public string Title { get; set; }
+
+	public string Description { get; set; }
+
+	public string Location { get; set; }
+
+	public DateTime StartAtUsc { get; set; }
+
+	public DateTime? EndAtUtc { get; set; }
+
+	public EventStatus Status { get; set; }
+}
