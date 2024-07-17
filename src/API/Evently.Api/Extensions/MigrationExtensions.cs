4dd05c6a0a762a8d08cc97eeae7d797f80e2cbed
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-07-17 17:23:30 +0200

    chore: Implement migration extensions
    We will use MigrationExtensions.cs to apply migrations on startup in development environment

---
+++ src/API/Evently.Api/Extensions/MigrationExtensions.cs
@@ -0,0 +1,20 @@
+using Evently.Modules.Events.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Evently.Api.Extensions;
+
+public static class MigrationExtensions
+{
+	internal static void ApplyMigrations(this IApplicationBuilder app)
+	{
+		using IServiceScope scope = app.ApplicationServices.CreateScope();
+		scope.ApplyMigration<EventDbContext>();
+	}
+
+	private static void ApplyMigration<TDbContext>(this IServiceScope scope)
+		where TDbContext : DbContext
+	{
+		using TDbContext context = scope.ServiceProvider.GetRequiredService<TDbContext>();
+		context.Database.Migrate();
+	}
+}
