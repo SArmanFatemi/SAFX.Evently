dbdd56614ead8780fa557c3dc6997667d89eccc4
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-07-22 23:03:03 +0200

    chore: Implement evently general exception

---
+++ src/Modules/Events/Evently.Modules.Events.Application/Abstractions/Exceptions/EventlyException.cs
@@ -0,0 +1,18 @@
+﻿using Evently.Modules.Events.Domain.Abstractions;
+using Evently.Modules.Events.Domain.Abstractions.Errors;
+
+namespace Evently.Modules.Events.Application.Abstractions.Exceptions;
+
+public sealed class EventlyException : Exception
+{
+    public EventlyException(string requestName, Error? error = default, Exception? innerException = default)
+        : base("Application exception", innerException)
+    {
+        RequestName = requestName;
+        Error = error;
+    }
+
+    public string RequestName { get; }
+
+    public Error? Error { get; }
+}
