42dfea35545e4661d88ca0daf517f9db744be151
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-07-22 23:14:40 +0200

    chore: Add category tag

--- src/Modules/Events/Evently.Modules.Events.Presentation/Tags.cs
+++ src/Modules/Events/Evently.Modules.Events.Presentation/Tags.cs
@@ -2,5 +2,7 @@ namespace Evently.Modules.Events.Presentation;
 
 internal static class Tags
 {
-	public const string Events = "Events";
+	internal const string Events = "Events";
+
+	internal const string Categories = "Categories";
 }
