72cfd1adcdd1534083426aeef59ef8a7f22dbc7b
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-07-23 11:52:34 +0200

    chore: Extend event domain

--- src/Modules/Events/Evently.Modules.Events.Domain/Events/DomainEvents/EventCreatedDomainEvent.cs
+++ src/Modules/Events/Evently.Modules.Events.Domain/Events/DomainEvents/EventCreatedDomainEvent.cs
@@ -1,8 +1,8 @@
 ﻿using Evently.Modules.Events.Domain.Abstractions;
 
-namespace Evently.Modules.Events.Domain.Events;
+namespace Evently.Modules.Events.Domain.Events.DomainEvents;
 
 public sealed class EventCreatedDomainEvent(Guid eventId) : DomainEvent
 {
-	public Guid EventId { get; init; } = eventId;
+    public Guid EventId { get; init; } = eventId;
 }
