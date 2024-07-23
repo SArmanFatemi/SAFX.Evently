9405906df247c522e7aaacea10c638e423916fb1
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-07-23 11:11:48 +0200

    chore: Implement ticket type endpoints

---
+++ src/Modules/Events/Evently.Modules.Events.Presentation/TicketTypes/Endpoints/ChangeTicketTypePriceEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Evently.Modules.Events.Application.TicketTypes.UpdateTicketTypePrice;
+using Evently.Modules.Events.Domain.Abstractions;
+using Evently.Modules.Events.Presentation.Abstractions.Endpoints;
+using Evently.Modules.Events.Presentation.ApiResults;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Events.Presentation.TicketTypes.Endpoints;
+
+internal class ChangeTicketTypePriceEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+    {
+        app.MapPut(TicketTypeEndpoints.BasePath + "/{id}/price", async (Guid id, Request request, ISender sender) =>
+            {
+                Result result = await sender.Send(new UpdateTicketTypePriceCommand(id, request.Price));
+
+                return result.Match(Results.NoContent, ApiResults.ApiResults.Problem);
+            })
+            .WithTags(Tags.TicketTypes);
+    }
+
+    internal sealed class Request
+    {
+        public decimal Price { get; init; }
+    }
+}
---
+++ src/Modules/Events/Evently.Modules.Events.Presentation/TicketTypes/Endpoints/GetTicketTypeEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Evently.Modules.Events.Application.TicketTypes.GetTicketType;
+using Evently.Modules.Events.Domain.Abstractions;
+using Evently.Modules.Events.Presentation.Abstractions.Endpoints;
+using Evently.Modules.Events.Presentation.ApiResults;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Events.Presentation.TicketTypes.Endpoints;
+
+internal class GetTicketTypeEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+    {
+        app.MapGet("ticket-types/{id}", async (Guid id, ISender sender) =>
+        {
+            Result<TicketTypeResponse> result = await sender.Send(new GetTicketTypeQuery(id));
+
+            return result.Match(Results.Ok, ApiResults.ApiResults.Problem);
+        })
+        .WithTags(Tags.TicketTypes);
+    }
+}
