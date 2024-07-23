28e98dfb7ece98f4149278ee8a8b37439b2c6365
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-07-23 10:33:51 +0200

    chore: Add ticket type to database context

--- src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/EventDbContext.cs
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/EventDbContext.cs
@@ -1,5 +1,6 @@
 using Evently.Modules.Events.Application.Abstractions.Data;
 using Evently.Modules.Events.Domain.Events;
+using Evently.Modules.Events.Domain.TicketTypes;
 using Microsoft.EntityFrameworkCore;
 
 namespace Evently.Modules.Events.Infrastructure.Database;
@@ -7,6 +8,8 @@ namespace Evently.Modules.Events.Infrastructure.Database;
 public sealed class EventDbContext(DbContextOptions<EventDbContext> options) : DbContext(options), IUnitOfWork
 {
 	internal DbSet<Event> Events { get; set; } = null!;
+	
+	internal DbSet<TicketType> TicketTypes { get; set; } = null!;
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
