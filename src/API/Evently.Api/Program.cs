54568609beb421bca8e92464d433626d0772aedd
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-07-24 14:28:54 +0200

    chore: Add custom schema to swagger

--- src/API/Evently.Api/Program.cs
+++ src/API/Evently.Api/Program.cs
@@ -4,7 +4,10 @@ using Evently.Modules.Events.Infrastructure.Database.Extensions;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+	options.CustomSchemaIds(t => t.FullName?.Replace("+", "."));
+});
 builder.Services.AddEventsModule(builder.Configuration);
 
 WebApplication app = builder.Build();
