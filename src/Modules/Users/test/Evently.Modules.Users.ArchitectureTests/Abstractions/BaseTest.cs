b52c08431f64ddb6f165eea70fdc70377bd014d9
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-08-12 14:14:44 +0200

    test: test: Implement architecture tests for user module

---
+++ src/Modules/Users/test/Evently.Modules.Users.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace Evently.Modules.Users.ArchitectureTests.Abstractions;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(Users.Domain.AssemblyReference).Assembly;
+
+    protected static readonly Assembly ApplicationAssembly = typeof(Users.Application.AssemblyReference).Assembly;
+
+    protected static readonly Assembly PresentationAssembly = typeof(Users.Presentation.AssemblyReference).Assembly;
+
+    protected static readonly Assembly InfrastructureAssembly = typeof(Users.Infrastructure.AssemblyReference).Assembly;
+}
