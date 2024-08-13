bf39f6edd7c19819de8bbedca1a87f2d21edc242
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-08-13 22:46:02 +0200

    test: Implement architecture tests for events module

---
+++ src/Modules/Events/test/Evently.Modules.Events.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace Evently.Modules.Events.ArchitectureTests.Abstractions;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly ApplicationAssembly = typeof(Events.Application.AssemblyReference).Assembly;
+
+    protected static readonly Assembly DomainAssembly = typeof(Events.Domain.AssemblyReference).Assembly;
+
+    protected static readonly Assembly InfrastructureAssembly = typeof(Events.Infrastructure.AssemblyReference).Assembly;
+
+    protected static readonly Assembly PresentationAssembly = typeof(Events.Presentation.AssemblyReference).Assembly;
+}
